44b100cb1542279060674e40976a29ab14840de4
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-09-02 09:08:15 -0300

    Função countAnimals concluída

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const todasEspecies = data.species.map((especie) => [
+      especie.name,
+      especie.residents.length,
+    ]);
+    return Object.fromEntries(todasEspecies);
+  }
+  if (animal.sex) {
+    return data.species
+      .find((especie) => especie.name === animal.specie)
+      .residents.filter((especie) => especie.sex === animal.sex).length;
+  }
+  return data.species.find((especie) => especie.name === animal.specie)
+    .residents.length;
 }
 
 module.exports = countAnimals;
