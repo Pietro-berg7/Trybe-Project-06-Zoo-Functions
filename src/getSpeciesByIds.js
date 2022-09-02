dcd41034a83c9462c09b78e33e8e65f19cc962ab
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-09-02 06:27:33 -0300

    Função getSpeciesByIds concluída

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return data.species.filter((animal) => ids.includes(animal.id));
 }
 
 module.exports = getSpeciesByIds;
