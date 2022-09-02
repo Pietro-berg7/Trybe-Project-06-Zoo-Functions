5efac63c3ad496dc02acbe9c983d840bb59a647f
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-09-02 06:58:27 -0300

    Função getAnimalsOlderThan concluída

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return data.species
+    .filter((especie) => especie.name === animal)
+    .every((especie) =>
+      especie.residents
+        .map((especie) => especie.age)
+        .every((idade) => idade >= age)
+    );
 }
 
 module.exports = getAnimalsOlderThan;
