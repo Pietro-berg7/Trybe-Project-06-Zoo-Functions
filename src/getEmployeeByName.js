cdb29ad528525960a9a54f1cfd7fe8021c95d1ca
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-09-02 07:11:23 -0300

    Função getEmployeeByName concluída

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  return employeeName
+    ? data.employees.find(
+        (name) =>
+          employeeName.includes(name.firstName) ||
+          employeeName.includes(name.lastName)
+      )
+    : {};
 }
 
 module.exports = getEmployeeByName;
