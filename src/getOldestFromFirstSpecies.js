c4b146c4d4e48f4fbc751866e74f396ee2140a8c
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-09-08 05:48:32 -0300

    Função getOldestFromFirstSpecies concluída

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,15 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const colaboradora = data.employees.find((element) => element.id === id);
+  const animal = colaboradora.responsibleFor[0];
+  const oldest = data.species
+    .find((element) => element.id === animal)
+    .residents.map((element) => element)
+    .reduce((element, element2) =>
+      element.age > element2.age ? element : element2
+    );
+  return [oldest.name, oldest.sex, oldest.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
